6688c180a232f9e63aed3122dd9157bddbf5d3c5
Sebastian Hojas <sebastian.hojas@irrelvant.at>
2016-01-04 11:38:49 +0100

    Added TestCases

---
+++ app/src/androidTest/java/localiser/LocaliserControllerTest.java
@@ -0,0 +1,16 @@
+package localiser;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by sebastian on 04/01/16.
+ */
+public class LocaliserControllerTest extends TestCase {
+
+    public void testTest()
+    {
+
+    }
+
+
+}
